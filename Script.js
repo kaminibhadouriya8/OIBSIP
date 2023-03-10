4de0e5e9cd8b951a96d401a9fb9c83cf719d4a10
Kamini Bhadouriya <127048135+kaminibhadouriya8@users.noreply.github.com>
2023-03-10 23:46:26 +0530

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,29 +1,18 @@
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
-const input = document.querySelector('input');
-const appBody = document.querySelector('.app-body');
-// EventListeners
-form.addEventListener("submit",(e) => {
-    e.preventDefault();
-    if(input.value.trim() != '') {
-        appBody.classList.add('app-body-there');
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <span class="value"><i class="fa-regular fa-circle-check circle"></i>${input.value.trim()}</span> <i class="fa-solid fa-trash del"></i>
-        `;
-        ul.append(li);
-        input.value = '';
+let outputscren = document.getElementById("outputscreen");
+function display(num) {
+    outputscren.value += num;
+}
+function calculate() {
+    try {
+        outputscren.value = eval(outputscren.value);
+    } catch (error) {
+        alert("Invalid");
     }
-});
-appBody.addEventListener("click",(e) => {
-    const element = e.target;
-    if(element.classList.contains('circle') || element.classList.contains('done-circle')) {
-        element.classList.toggle('circle');
-        element.classList.toggle('done-circle');
-        element.parentElement.classList.toggle('todo-done');
-    }
-    else if(element.classList.contains('del')) {
-        element.parentElement.remove();
-    }
-    if(appBody.innerText == '') appBody.classList.remove('app-body-there');
-});
+}
+function Clear() {
+    outputscren.value = "";
+}
+function del() {
+    // delete 1 letter at a time
+    outputscren.value = outputscren.value.slice(0, -1);
+}
